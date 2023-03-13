3ce7b9e4e6590207fe7252b38961d9aa6867d1d3
Sosobae <trulytru404@gmail.com>
2023-03-13 13:48:41 -0700

    l

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-for i in matrix:
-print(" ".join("{:d}".format(j) for j in i))
+for element in matrix:
+for number in element:
+print("{:d}".format(number), end="")
+if(number is not element[len(element)-1]):
+print(" ", end="")
+print()
 
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,4 +3,4 @@ def max_integer(my_list=[]):
 if(len(my_list) == 0):
 return(None)
 return(sorted(my_list)[-1])
-
+                
